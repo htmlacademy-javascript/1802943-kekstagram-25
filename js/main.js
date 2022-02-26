542bc8bfa7542aa1d20c5c84e764019e946ce59f
AlterRusse <Arkhsocial@gmail.com>
2022-02-26 16:05:30 +0300

    Второе исправление

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 //Первая функция
 
-function getRandofromclusiveInteger(from, to) {
+function getRandomInclusiveInteger(from, to) {
   //Гвард (нет числовых значений || два отрицательных значения)
   if (typeof from !== 'number' || typeof to !== 'number' || from < 0 && to < 0) {
     return null;
+  }
+
+  //Одно отрицательное число
+  if (from < 0) {
+    from = 0;
+  }
+  if (to < 0) {
+    to = 0;
   }
   //Числа равны
   if (from === to) {
     return from;
   }
-  //Число
-  if (from < 0 && to > 0) {
-    from = 0;
-  }
-
-  if (to < 0 && from > 0) {
-    to = 0;
-  }
   //Диапазон перевернут
   if (from > to) {
-    [from, to] = [to, from];
+    [from, to] = [to, from]; //деструктурирующее присваивание
   }
 
   return Math.round(Math.ceil(from) + Math.random() * (Math.floor(to) - Math.ceil(from) + 1));
 }
 
-getRandofromclusiveInteger();
+getRandomInclusiveInteger();
 
 //Вторая функция
 
