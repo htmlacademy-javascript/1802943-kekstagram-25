928ef6900f59d3b1738266dfa621f1e2b16aea73
AlterRusse <Arkhsocial@gmail.com>
2022-03-14 00:20:06 +0300

    промежуточная версия 1

--- js/main.js
+++ js/main.js
@@ -39,3 +39,57 @@ function isValidStringLength (line = '', maxWidth) {
 }
 
 isValidStringLength('строка', 6);
+
+const commentsArray = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const namesArray = [
+  'Толян',
+  'Борисыч',
+  'Джульетта',
+  'Дед Максим'
+];
+
+const descriptionArray = [
+  'Красивое',
+  'Пойдет',
+  'Не красивое',
+  'Огонь'
+];
+
+const getRandomArrayElement = (elements) => elements[getRandomInclusiveInteger(0, elements.length - 1)];
+
+function photoDescription (_, countPosition) {
+  const randomImage = countPosition + 1;
+
+  return {
+    id: randomImage,
+    url: 'photos/&{randomImage}.jpg',
+    description: getRandomArrayElement(descriptionArray),
+    likes: getRandomInclusiveInteger(15, 200),
+    comment: Array.from({length:getRandomInclusiveInteger(1, 10)}, commentsList)
+  };
+}
+
+function createIdGenerator () {
+  let lastGeneratedId = 0;
+  return function () {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+}
+
+function commentsList () {
+  return {
+    id: createIdGenerator(),
+    avatar: 'img/avatar-{getRandomInclusiveInteger (1, 6)}.svg',
+    message: getRandomArrayElement(commentsArray),
+    name: getRandomArrayElement(namesArray)
+  };
+}
