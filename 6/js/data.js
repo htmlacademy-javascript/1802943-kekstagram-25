d5a45905b2b3319f3db852bf9466216d281f79ea
Keks <keks@htmlacademy.ru>
2022-03-30 12:27:52 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/data.js
@@ -0,0 +1,44 @@
+const POSTS_QUANTITY = 25;
+
+const USERS_COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const USERS_NAMES = [
+  'Толян',
+  'Борисыч',
+  'Джульетта',
+  'Дед Максим',
+  'Хисока'
+];
+
+const PHOTO_DESCRIPTIONS = [
+  'Красивое',
+  'Пойдет',
+  'Не красивое',
+  'Огонь',
+  'Удалить немедленно'
+];
+
+const MIN_COMMENT_ID = 1;
+const MAX_COMMENT_ID = 999;
+
+const MIN_AVATAR_NUMBER = 1;
+const MAX_AVATAR_NUMBER = 6;
+
+const MIN_LIKES_QUANTITY = 15;
+const MAX_LIKES_QUANTITY = 200;
+
+const MIN_COMMENT_NUMBER = 1;
+const MAX_COMMENT_NUMBER = 6;
+
+export {
+  POSTS_QUANTITY, USERS_COMMENTS, USERS_NAMES, PHOTO_DESCRIPTIONS,
+  MIN_COMMENT_ID, MAX_COMMENT_ID, MIN_AVATAR_NUMBER, MAX_AVATAR_NUMBER,
+  MIN_LIKES_QUANTITY, MAX_LIKES_QUANTITY, MIN_COMMENT_NUMBER, MAX_COMMENT_NUMBER
+};
---
+++ 6/js/main.js
@@ -0,0 +1,6 @@
+import {usersPosts} from './users-posts.js';
+// eslint-disable-next-line
+import {renderUserThumbnails} from './thumbnails-rendering.js';
+
+// eslint-disable-next-line
+console.log(usersPosts);
